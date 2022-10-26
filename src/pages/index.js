14b5b26e674551f026dcfd657e9c8749d726bab4
Juan Brundi Sarmento Madeira <juanbrundism@gmail.com>
2022-10-26 23:00:10 +0700

    Create navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import useTranslate from "../hooks/useTranslate";
 
+//Components
+import { Navbar } from "../components";
+
+//Pages
 import homepage from "./homepage";
 import product from "./product";
 
@@ -10,17 +14,14 @@ const appPages = [{ ...homepage }, { ...product }];
 const RenderPages = () => {
   const { changeLanguage, language } = useTranslate();
   return (
-    <>
-      <div style={{ marginBottom: "20px" }}>
-        <div>TEST LANGUAGE - {language}</div>
-        <button onClick={changeLanguage}>change</button>
-      </div>
+    <div style={{ width: "100vw" }}>
+      <Navbar />
       <Routes>
         {appPages.map((page, i) => (
           <Route {...page} key={i} />
         ))}
       </Routes>
-    </>
+    </div>
   );
 };
 
